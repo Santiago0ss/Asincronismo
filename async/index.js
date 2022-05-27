45462e71512b8de534812882da6114d024f06439
Santiago Salazar <santiago1234ocampo@gmail.com>
2022-05-27 18:02:31 -0300

    trabajando con asincronismo usan async

---
+++ async/index.js
@@ -0,0 +1,29 @@
+const doSomethungAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ?setTimeout(() => resolve('Do Something Async'), 3000)
+        : reject(new Error('Test error'))
+    });
+}
+
+const doSomething = async () => {
+    const something = await doSomethungAsync();
+    console.log(something);
+}
+
+console.log('before');
+doSomething();
+console.log('after');
+
+const anotherFunction = async () => {
+    try {
+        const something = await doSomethungAsync();
+        console.log(something);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+console.log('before 1');
+anotherFunction();
+console.log('after 1');
